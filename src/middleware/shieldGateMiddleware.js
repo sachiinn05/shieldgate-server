5c2b8c1964bd58aada672154f62a22b90ab68064
sachinsingh <sachinsingh6386@gmail.com>
2026-04-15 19:56:46 +0530

    feat: implement in-memory caching in middleware to optimize rate limiting performance

--- src/middleware/shieldGateMiddleware.js
+++ src/middleware/shieldGateMiddleware.js
@@ -1,5 +1,6 @@
 const axios=require("axios");
-
+const cache={};
+const CACHE_TTL=5000;
 const shieldGate=()=>{
     return async (req,res,next)=>
     {
@@ -10,20 +11,50 @@ const shieldGate=()=>{
 
         const ip=req.ip|| req.connection.remoteAddress || req.header["x-forwarded-for"];
 
+        const now=Date.now();
+
+        if(cache[ip] && cache[ip].expiry>now)
+        {
+            console.log("CACHE HIT");
+            
+        
+        if(!cache[ip].allowed)
+        {
+            return res.status(429).json({
+                allowed:false,
+                message:"Two many requests (cached)",
+            });
+        }
+        return next();
+    }
+    console.log("CACHE MISS");
+    
+
         const response=await axios.post(
             "http://localhost:8001/api/check-request",
             {ip},
             {timeout:500}
         );
-        if(!response.data.allowed)
+
+        const data=response.data;
+        cache[ip]={
+            allowed:data.allowed,
+            expiry:now+CACHE_TTL
+        };
+        if(!data.allowed)
         {
-            return res.status(429).json(response.data);
+            return res.status(429).json(data);
 
         }
         next();
       }catch(error)
       {
+        if (error.response && error.response.status === 429) {
+        return res.status(429).json(error.response.data);
+        }
+
         console.error("Middleware Error:", error.message);
+
         next();
       }
     };
